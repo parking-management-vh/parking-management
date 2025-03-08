020d9a0e663133249fcb2a4fd714e1335127843d
HaiBarca03 <doanduchai8d2m@gmail.com>
2025-03-09 00:41:41 +0700

    create feature frm vehicle admin

--- ParkingManagement/GUI/Program.cs
+++ ParkingManagement/GUI/Program.cs
@@ -17,7 +17,7 @@ namespace ParkingManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmParkingCard());
+            Application.Run(new frmLogin());
         }
     }
 }
